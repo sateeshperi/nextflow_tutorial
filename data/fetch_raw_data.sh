34a339fc93acd8246493254d3bc838d067eac53b
Sateesh <33637490+sateeshperi@users.noreply.github.com>
2022-02-03 10:07:12 -0500

    Update fetch_raw_data.sh

--- data/fetch_raw_data.sh
+++ data/fetch_raw_data.sh
@@ -1,5 +1,6 @@
-mkdir untrimmed_fastq/
-cd untrimmed_fastq/
+# Download raw data
+mkdir -p /workspace/nextflow_tutorial/data/untrimmed_fastq/
+cd /workspace/nextflow_tutorial/data/untrimmed_fastq/
 
 curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_1.fastq.gz
 curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_2.fastq.gz
@@ -7,3 +8,10 @@ curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_1.fa
 curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_2.fastq.gz
 curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_1.fastq.gz
 curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_2.fastq.gz
+
+# Download reference genome
+mkdir -p /workspace/nextflow_tutorial/data/ref_genome
+cd /workspace/nextflow_tutorial/data/ref_genome
+curl -L -o /workspace/nextflow_tutorial/data/ref_genome/ecoli_rel606.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/017/985/GCA_000017985.1_ASM1798v1/GCA_000017985.1_ASM1798v1_genomic.fna.gz
+gunzip /workspace/nextflow_tutorial/data/ref_genome/ecoli_rel606.fasta.gz
+ls
