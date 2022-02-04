af068a9aa110f8ecd923f0e699e26e3ecd333001
Sateesh <33637490+sateeshperi@users.noreply.github.com>
2022-02-04 11:48:25 -0500

    Update envconfig.sh

--- envconfig.sh
+++ envconfig.sh
@@ -1,2 +1,6 @@
-#Install conda packages 
+# Export NXF_OPTS
+unset JAVA_TOOL_OPTIONS
+export NFX_OPTS=$JAVA_TOOL_OPTIONS
+
+# Create `varcal` env with conda packages 
 mamba env create -n varcal -f environment.yml
