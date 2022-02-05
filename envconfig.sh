8c30c576ef7fb23917cccde2a351f336b04f2746
Sateesh <33637490+sateeshperi@users.noreply.github.com>
2022-02-04 19:32:06 -0500

    Update envconfig.sh

--- envconfig.sh
+++ envconfig.sh
@@ -1,9 +1,10 @@
-conda init bash
-source ~/.bashrc
-
 # Export NXF_OPTS
 unset JAVA_TOOL_OPTIONS
 export NFX_OPTS=$JAVA_TOOL_OPTIONS
 
 # Create `varcal` env with conda packages 
 mamba env create -n varcal -f environment.yml
+
+# Conda init
+conda init bash
+echo "Run `source ~/.bashrc` => to restart terminal"
